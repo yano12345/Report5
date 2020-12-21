17de36851dae498373e0435a6b11e902c74673e7
Yano Kotaro <e205748@ie.u-ryukyu.ac.jp>
2020-12-22 05:40:16 +0900

    1st commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e205748/Main.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e205748;
+
+public class Main {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }        
+}
