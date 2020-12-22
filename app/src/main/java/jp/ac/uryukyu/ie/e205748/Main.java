0a5ee0fc2122a3bb078e9b1e1fc5452b93e9bd40
yano12345 <74218139+yano12345@users.noreply.github.com>
2020-12-22 19:34:08 +0900

    Update Main.java

--- app/src/main/java/jp/ac/uryukyu/ie/e205748/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205748/Main.java
@@ -2,11 +2,6 @@ package jp.ac.uryukyu.ie.e205748;
 
 public class Main {
     public static void main(String[] args){
-        String str = null;
-        try{
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExceptionが発生しました");
-            System.outprintln(e.getMessage());
-    }        
+        String str = "百二十三";
+        int value = Integer.parseInt(str); 
 }
